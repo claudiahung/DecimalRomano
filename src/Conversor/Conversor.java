a389e13754790901aaf1b23eea26ad5603485f5d
claudia <claudiafhung@gmail.com>
2016-08-29 19:54:30 +0200

    criamos controller, mais testes

--- src/Conversor/Conversor.java
+++ src/Conversor/Conversor.java
@@ -1,18 +1,46 @@
 package Conversor;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class Conversor {
-	
-	public Conversor()
-	{
-		
+
+	@SuppressWarnings("serial")
+	private static Map<Character, Integer> tabela = new HashMap<Character, Integer>() {
+		{
+			put('I', 1);
+			put('V', 5);
+			put('X', 10);
+			put('L', 50);
+			put('C', 100);
+			put('D', 500);
+			put('M', 1000);
+		}
+	};
+
+	public Conversor() {
+
 	}
-	
-	public int conversao(String r)
-	{
-		return 0;
+
+	public int conversao(String r) {
+		int acumulador = 0;
+		int ultimoVizinhoDaDireita = 0;
+
+		for (int i = r.length() - 1; i >= 0; i--) {
+			// pega o inteiro referente ao simbolo atual
+			int atual = tabela.get(r.charAt(i));
+
+			// se o da direita for menor, o multiplicaremos
+			// por -1 para torná-lo negativo
+			int multiplicador = 1;
+			if (atual < ultimoVizinhoDaDireita)
+				multiplicador = -1;
+			acumulador += tabela.get(r.charAt(i)) * multiplicador;
+
+			// atualiza o vizinho da direita
+			ultimoVizinhoDaDireita = atual;
+		}
+		return acumulador;
 	}
-	
-	
 
 }
--- src/Test/ConversorTest.java
+++ src/Test/ConversorTest.java
@@ -1,5 +1,8 @@
 package Test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +27,64 @@ public class ConversorTest {
 
 		Assert.assertEquals(1, result);
 	}
+	
+	@Test
+	public void converterOSimboloV() {
+
+		
+
+		int resultadoObtido = c.conversao("V");
+
+		assertEquals(5, resultadoObtido);
+	}
+
+	@Test
+	public void converterOSimboloC() {
+
+
+		int resultadoObtido = c.conversao("C");
+
+		assertEquals(100, resultadoObtido);
+	}
+
+	@Test
+	public void converterOSimboloII() {
+		
+		int resultadoObtido = c.conversao("II");
+
+		assertEquals(2, resultadoObtido);
+	}
+
+	@Test
+	public void converterOSimboloIV() {
+	
+		int resultadoObtido = c.conversao("IV");
+
+		assertEquals(4, resultadoObtido);
+	}
+
+	@Test
+	public void converterOSimboloIX() {
+	
+		int resultadoObtido = c.conversao("IX");
+
+		assertEquals(9, resultadoObtido);
+	}
+
+	@Test
+	public void converterOSimboloXXIV() {
+		
+		int resultadoObtido = c.conversao("XXIV");
+
+		assertEquals(24, resultadoObtido);
+	}
+	
+	@After
+	public void Finaliza() {
+		System.out.println("Finaliza");
+		
+		}
+
 	
 
 
